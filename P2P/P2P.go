a5dee819f04535d0a1724b9f7c47e07a2e1a7eee
liamm <49158467+peterrrre@users.noreply.github.com>
2020-02-13 18:43:34 +0800

    Create P2P.go

---
+++ P2P/P2P.go
@@ -0,0 +1,13 @@
+package runner
+
+import (
+	"time"
+)
+
+const DefaultStartTimeout = 45 * time.Second
+
+// DefaultNamer is the default naming function.
+var DefaultNamer NameFunc = helpers.UniqueName
+
+// A NameFunc is used to generate container names.
+type NameFunc func() string
